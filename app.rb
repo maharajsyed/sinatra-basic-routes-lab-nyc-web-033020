16037cd671f2e742f9a4ffe7418238c0e96b1bd8
maharajsyed <maharajsyed@gmail.com>
2020-04-21 13:15:23 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do
+        "My name is Maharaj"
+    end
+
+    get '/hometown' do
+        "My hometown is Sugarland"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Sky Full of Stars"
+    end
+
 end
